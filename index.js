7dae5f32aa8c7a91bdf0fc979b8605a4b1dda6c1
Gustavo Rodriguez <gustavo@bengala.tech>
2021-11-03 10:04:56 -0500

    feat: Add yarn workspace support and usage in nested addons

--- index.js
+++ index.js
@@ -3,12 +3,27 @@
 var path = require("path");
 var Funnel = require("broccoli-funnel");
 var MergeTrees = require("broccoli-merge-trees");
+const findWorkspaceRoot = require('find-yarn-workspace-root');
 
 module.exports = {
   name: require("./package").name,
+  _nodeModulesPath: null,
 
   included: function(app) {
     this._super.included.apply(this, arguments);
+
+    let current = this;
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+    
+    this.app = app;
+
+    const workspaceRoot = findWorkspaceRoot(this.app.project.root);
+    const root = workspaceRoot || this.app.project.root;
+    this._nodeModulesPath = path.join(root, "node_modules");
 
     app.import("vendor/jsoneditor.css");
     app.import("vendor/jsoneditor-icons.svg", {
@@ -18,14 +33,14 @@ module.exports = {
 
   treeForVendor(/* vendorTree */) {
     let cssTree = new Funnel(
-      path.join(this.project.root, "node_modules", "jsoneditor/dist"),
+      path.join(this._nodeModulesPath, "jsoneditor/dist"),
       {
         files: ["jsoneditor.css"]
       }
     );
 
     let iconTree = new Funnel(
-      path.join(this.project.root, "node_modules", "jsoneditor/dist/img"),
+      path.join(this._nodeModulesPath, "jsoneditor/dist/img"),
       {
         files: ["jsoneditor-icons.svg"]
       }
